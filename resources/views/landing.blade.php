6a6a359d6758f987a0dcbebde7c8b67934b84106
Jacob Bennett <me@jakebennett.net>
2017-01-18 13:17:47 -0600

    feat: google analytics

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -11,6 +11,16 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{elixir('css/app.css')}}">
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-51293444-5', 'auto');
+            ga('send', 'pageview');
+
+        </script>
 
     </head>
     <body class="bgg10">
